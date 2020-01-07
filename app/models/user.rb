c0ba6ca163ff4ad4947aaa5c4d0f7d4f5a1893fe
hung <hungtranpro2@gmail.com>
2020-01-07 15:48:29 +0700

    chapter_8: Basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,14 @@ class User < ApplicationRecord
   before_save :email_downcase
   has_secure_password
 
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+  end
+
   private
 
   def email_downcase
