9c205a402aeb6455a76ed93114545188ce4b7949
hung <hungtranpro2@gmail.com>
2020-01-07 15:48:29 +0700

    chapter_7: Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-
   validates :email, format: {with: Settings.valid_email_regex}, presence: true,
-   length: {maximum: Settings.max_email}, uniqueness: {case_sensitive: false}
+    length: {maximum: Settings.max_email}, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: Settings.min_pass}
   validates :name, presence: true, length: {maximum: Settings.max_name}
   before_save :email_downcase
